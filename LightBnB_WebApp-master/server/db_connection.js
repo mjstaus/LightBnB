2a16e46f0bed02bc820c234405eaa7a9872642fb
Martha Staus <mjstaus@gmail.com>
2022-01-19 05:50:01 -0800

    Add environmental variables with dotenv

---
+++ LightBnB_WebApp-master/server/db_connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+
+console.log(`Connecting to db: ${process.env.DB_NAME}💁‍♀️🏘`);
+
+module.exports = pool;
